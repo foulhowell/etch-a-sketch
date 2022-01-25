1fe151665a6ac9189e3a1a605e7c1c4507175362
Noah Howell <nhowell117@gmail.com>
2022-01-25 12:52:25 -0500

    Added warning for exceeding box limit, rainbow mode, and fixed sizing of boxes.

--- eas-code.js
+++ eas-code.js
@@ -1,4 +1,6 @@
 const container = document.querySelector('#container');
+const rainbowButton = document.querySelector('#rainbow-button');
+let rainbowModeOn = false;
 
 function makeGrid(numBoxes) {
     for (let i = 0; i < numBoxes; i++) {
@@ -7,20 +9,61 @@ function makeGrid(numBoxes) {
         for (let j = 0; j  < numBoxes; j++) {
             const box = document.createElement('div');
             box.classList.add('box');
+            // let boxSize = 100/numBoxes;
+            // box.style.width(`20px`);
+            // box.style.height(`20px`);
             row.appendChild(box);
         }
         container.appendChild(row);
     }
+    colorBox();
 }
 
 function colorBox() {
     const boxes = document.querySelectorAll('.box');
     boxes.forEach((box) => {
         box.addEventListener('mouseover', function (e) {
-            e.target.style.backgroundColor = "black";
+            if (rainbowModeOn) {
+                let rgb = randomColor();
+                e.target.style.backgroundColor = `rgb(${rgb[0]},${rgb[1]},${rgb[2]})`;
+            }
+            else {
+                e.target.style.backgroundColor = "black";
+            }
         });
     });
 }
 
+function resetGrid() {
+    let newNumBoxes = parseInt(prompt("How many boxes of rows/columns would you like (Max is 100)?", 16));
+    if (newNumBoxes < 101) {
+        let rows = document.getElementsByClassName('row');
+        while(rows.length > 0) {
+            rows[0].parentNode.removeChild(rows[0]);
+        }   
+        makeGrid(newNumBoxes);
+    }
+    else {
+        alert('Please input a number between 1 and 100.');
+    }
+
+}
+
+function activateRainbowMode() {
+    if (rainbowModeOn) {
+        rainbowButton.textContent = 'Rainbow Mode: Off';
+    }
+    else {
+        rainbowButton.textContent = 'Rainbow Mode: On';
+    }
+    rainbowModeOn = !rainbowModeOn;
+}
+
+function randomColor() {
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    return [red, green, blue];
+}
+
 makeGrid(16);
-colorBox();
